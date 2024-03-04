a52ea8a21e8c0fa10abc37f27287bb5fc008c697
AndersPanders123 <123541105+AndersPanders123@users.noreply.github.com>
2024-03-04 14:09:00 +0100

    faean jeg skal drepe hele github, de er en gjebng medc negenryane jeg r hjharte rgi9thub

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<!-- MyComponent.svelte -->
+
+<script>
+  let counter = 0;
+
+  const increaseCounter = () => {
+    counter += 10;
+  };
+
+  const decreaseCounter = () => {
+    counter -= 10;
+  };
+</script>
+
+<div class="row">
+  <div class="button">
+    <button on:click={increaseCounter}>+</button>
+  </div>
+  <div class="button">
+    <button on:click={decreaseCounter}>-</button>
+  </div>
+</div>
+
+<p>Tempratur: {counter}*C</p>
+
+<style>
+  /* Your styles for the component go here */
+  .row {
+    display: flex;
+    flex-direction: row;
+    /* Add any additional styling as needed */
+  }
+
+  .button {
+    /* Styling for the button container */
+    margin-right: 10px; /* Adjust as needed */
+  }
+</style>
